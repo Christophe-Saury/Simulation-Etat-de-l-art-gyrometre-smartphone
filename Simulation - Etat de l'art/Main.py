cd0888872a1e51d585717afb7881990b082943a8
Christophe-Saury <95085659+Christophe-Saury@users.noreply.github.com>
2021-12-08 12:05:10 +0100

    Update Main.py

--- Simulation - Etat de l'art/Main.py
+++ Simulation - Etat de l'art/Main.py
@@ -1,7 +1,18 @@
-#import kivy
-from kivy.app import App
 
-class MyApp(App):
-    pass
 
-MyApp.run()
+class Main:
+    
+    
+    
+    def Initialize():
+        rot_x = input("Enter value of rotation of x axis: ")
+        rot_y = input("Enter value of rotation of y axis: ")
+        rot_z = input("Enter value of rotation of z axis: ")
+        duration = input("Enter duration of the operation: ")
+
+
+    def run_main():
+        Initialize():
+
+
+
